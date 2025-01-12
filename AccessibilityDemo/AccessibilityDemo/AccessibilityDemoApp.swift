5b0a1ac38bebe688f3d4943b3469e8effb4a5937
Pablo Gómez <pablo.gof@gmail.com>
2025-01-12 22:11:52 +0100

    Add quick actions

--- AccessibilityDemo/AccessibilityDemo/AccessibilityDemoApp.swift
+++ AccessibilityDemo/AccessibilityDemo/AccessibilityDemoApp.swift
@@ -7,6 +7,10 @@ import SwiftUI
 
 @main
 struct AccessibilityDemoApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    @Environment(\.scenePhase) var scenePhase
+    
     @Environment(\.colorScheme) var colorScheme
     @Environment(\.dynamicTypeSize) var dynamicTypeSize
     
@@ -23,6 +27,18 @@ struct AccessibilityDemoApp: App {
             RootView()
                 .onAppear {
                     getAccessibilitySettings()
+                }
+                .onChange(of: scenePhase) { _, newPhase in
+                    switch newPhase {
+                    case .background:
+                        addQuickActions()
+                    case .inactive:
+                        break
+                    case .active:
+                        break
+                    @unknown default:
+                        break
+                    }
                 }
         }
     }
@@ -46,5 +62,66 @@ struct AccessibilityDemoApp: App {
         
         print("region: \(Locale.current.region?.identifier ?? "Unknown")")
         print("language: \(Locale.current.language.languageCode ?? "Unknown")")
+    }
+    
+    func addQuickActions() {
+        let newNoteAction = UIApplicationShortcutItem(
+            type: "com.demo.AccessibilityDemo.newTransfer",
+            localizedTitle: NSLocalizedString("quickaction.newTransfer.title", comment: "Title for new transfer quick action"),
+            localizedSubtitle: nil,
+            icon: UIApplicationShortcutIcon(type: .compose)
+        )
+        
+        let searchAction = UIApplicationShortcutItem(
+            type: "com.demo.AccessibilityDemo.searchATM",
+            localizedTitle: NSLocalizedString("quickaction.searchATMs.title", comment: "Title for search ATMs quick action"),
+            localizedSubtitle: nil,
+            icon: UIApplicationShortcutIcon(type: .search)
+        )
+        
+        UIApplication.shared.shortcutItems = [newNoteAction, searchAction]
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        if let shortcutItem = options.shortcutItem {
+            QuickActionsManager.shared.handleQaItem(shortcutItem)
+        }
+        
+        let sceneConfiguration = UISceneConfiguration(name: "Custom Configuration", sessionRole: connectingSceneSession.role)
+        sceneConfiguration.delegateClass = SceneDelegate.self
+        
+        return sceneConfiguration
+    }
+    
+    func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        
+        completionHandler(true)
+    }
+}
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    func windowScene(_ windowScene: UIWindowScene, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        QuickActionsManager.shared.handleQaItem(shortcutItem)
+    }
+}
+
+class QuickActionsManager: ObservableObject {
+    
+    static let shared = QuickActionsManager()
+
+    func handleQaItem(_ shortcutItem: UIApplicationShortcutItem) {
+        // TODO: Observe these notifications and trigger actions
+        switch shortcutItem.type {
+        case "com.demo.AccessibilityDemo.newTransfer":
+            NotificationCenter.default.post(name: .newTransferQuickAction, object: nil)
+        case "com.demo.AccessibilityDemo.searchATM":
+            NotificationCenter.default.post(name: .searchATMsQuickAction, object: nil)
+        default:
+            break
+        }
     }
 }
---
+++ AccessibilityDemo/AccessibilityDemo/Extensions/Notification+Custom.swift
@@ -0,0 +1,11 @@
+//
+//  Notification+Custom.swift
+//  AccessibilityDemo
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let newTransferQuickAction = Notification.Name("newTransferQuickAction")
+    static let searchATMsQuickAction = Notification.Name("searchATMsQuickAction")
+}
